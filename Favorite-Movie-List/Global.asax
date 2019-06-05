9191789b6b520cc1f22c1fce54480cdff0f6ff3f
Eric Holt <fatmoti@gmail.com>
2019-06-05 13:15:11 -0400

    Initial Commit

---
+++ Favorite-Movie-List/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Favorite_Movie_List.MvcApplication" Language="C#" %>
---
+++ Favorite-Movie-List/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Favorite_Movie_List.Startup))]
+namespace Favorite_Movie_List
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
