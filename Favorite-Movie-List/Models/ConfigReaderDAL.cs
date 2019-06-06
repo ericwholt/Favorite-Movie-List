9e24965992d7a307f1519bdad6c75ce7992ef819
Eric Holt <fatmoti@gmail.com>
2019-06-06 09:14:47 -0400

    Started Config Manager DAL

---
+++ Favorite-Movie-List/Models/ConfigReaderDAL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Favorite_Movie_List.Models
+{
+    public class ConfigReaderDAL
+    {
+        static string ReadSetting(string key)
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                return appSettings[key] ?? "Not Found";
+            }
+            catch (ConfigurationErrorsException)
+            {
+                return "Unable to access file";
+            }
+        }
+    }
+}
